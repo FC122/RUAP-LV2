671f311f8df36017caa37f007c803b6819b3c5f1
filip <filip.cica1234@gmail.com>
2022-11-23 15:52:48 +0100

    changes

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ContactManager.Services;
+using System.Net.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        // GET: Contact
+        public Contact[] Get()
+        {
+            return this.contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
